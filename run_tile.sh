67ad40f9c6dc6546d86c11b2363432d798afe6a7
Hugo VERNET <hugo.vernet@student.junia.com>
2024-12-05 14:11:16 +0100

    ca marche

--- run_tile.sh
+++ run_tile.sh
@@ -1,15 +1,15 @@
 python run_infer.py \
 --gpu='0' \
---nr_types=6 \
+--nr_types=3 \
 --type_info_path=type_info.json \
 --batch_size=64 \
 --model_mode="fast" \
---model_path='C:/Program Files/Git/hovernet_fast_pannuke_type_tf2pytorch.tar' \
+--model_path='C:/Program Files/Git/he.tar' \
 --nr_inference_workers=8 \
 --nr_post_proc_workers=16 \
 tile \
 --input_dir="C:\Program Files\Git\image" \
---output_dir="C:\Users\verne\Documents\GitHub\hover_net2\resultat" \
+--output_dir="C:\Users\verne\Documents\GitHub\holly_net\resultat" \
 --mem_usage=0.1 \
 --draw_dot \
 --save_qupath \
